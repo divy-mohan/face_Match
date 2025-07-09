011dcc974ae0e9eef9889f285e56c1cddb441cc0
divy-mohan <divymohan.awgp@gmail.com>
2025-07-09 17:27:15 +0000

    Fix: moved WhiteNoiseMiddleware to MIDDLEWARE section

--- face_attendance/settings.py
+++ face_attendance/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'core',  # Custom app for core functionality
     'rest_framework',  # Django REST Framework for API development
 ]
